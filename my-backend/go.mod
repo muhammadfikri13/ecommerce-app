960d689ad76546b4a74bef55eeb72d1d8bdd5e37
muhammadfikri13 <muhammadfikri.mf457@gmail.com>
2024-07-22 16:15:43 +0700

    initial commit

---
+++ my-backend/go.mod
@@ -0,0 +1,3 @@
+module my-backend
+
+go 1.22.5
---
+++ my-backend/routes/routes.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"EcommerceApp/my-backend/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) { // Routing for User and Admin
+	incomingRoutes.POST("/users/signup", controllers.Signup())
+	incomingRoutes.POST("/users/login", controllers.Login())
+	incomingRoutes.POST("/admin/addProduct", controllers.ProductViewerAdmin())
+	incomingRoutes.GET("/users/productView", controllers.SearchProduct())
+	incomingRoutes.GET("/users/search", controllers.SearchProductByQuery())
+
+}
